b81d80c255eb2007387f76c15e94e2ee50cd9ef8
Kavia AI <production@kavia.ai>
2025-11-18 04:11:52 +0000

    Checkpoint 4 - cg8188f997

--- portfolio_frontend/src/index.js
+++ portfolio_frontend/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import RouterApp from './RouterApp';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterApp />
+    <App />
   </React.StrictMode>
 );
